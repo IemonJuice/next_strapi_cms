d141b369deddbc32a08c2bcdd366d32b066a6170
payu <pavlo.yurchenko@amedia.ua>
2026-04-18 18:15:09 +0300

    finish 7 parts

---
+++ backend/src/api/summary/middlewares/on-summary-create.ts
@@ -0,0 +1,38 @@
+import type { Core } from "@strapi/strapi";
+
+export default (config, { strapi }: { strapi: Core.Strapi }) => {
+  return async (ctx, next) => {
+    const user = ctx.state.user;
+    if (!user) return ctx.unauthorized("You are not authenticated");
+
+    const availableCredits = user.credits;
+    if (availableCredits === 0)
+      return ctx.unauthorized("You do not have enough credits.");
+
+
+    const modifiedBody = {
+      ...ctx.request.body,
+      data: {
+        ...ctx.request.body.data,
+        authorId: ctx.state.user.documentId,
+      },
+    };
+
+
+    ctx.request.body = modifiedBody;
+
+    await next();
+
+    try {
+      await strapi.documents("plugin::users-permissions.user").update({
+        documentId: user.documentId,
+        data: {
+          credits: availableCredits - 1,
+        },
+      });
+    } catch (error) {
+      ctx.badRequest("Error Updating User Credits");
+    }
+
+  };
+};
